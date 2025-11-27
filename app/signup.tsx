84681f59398f07d4fef400b1301627a182333b52
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2025-11-27 15:47:42 +0100

    feat: update login screen and form button for sign in functionality

--- app/signup.tsx
+++ app/signup.tsx
@@ -4,35 +4,41 @@ import icons from "@/constants/icons";
 import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function Index() {
+export default function Signup() {
   return (
     <SafeAreaView className="flex-1 bg-white pt-8 px-5">
       <TouchableOpacity>
         <Image source={icons.arrowleft} className="h-[15px] w-[18px] mb-4" />
       </TouchableOpacity>
-      <Text className="text-[24px] font-poppins-semibold">Sign In</Text>
+      <Text className="text-[24px] font-poppins-semibold">Create Account</Text>
       <Text className="text-[16px] font-poppins text-gray-1">
-        Login to your account
+        Setup your account to get started
       </Text>
       {/* inputs */}
       <View className="mt-6">
-        <FormInput title="Email address" placeholder="Email address" />
-        <FormInput title="Password" placeholder="Password" eye={true} />
+        <FormInput title="Your Full name" placeholder="Enter your full name" />
+        <FormInput title="Your Email address" placeholder="Email address" />
+        <FormInput title="Create Password" placeholder="Password" eye={true} />
       </View>
       {/* privacy and policy */}
       <View className="flex-row items-center mb-12">
-        <Image source={icons.box} className="h-[18px] w-[18px] mr-3" />
-        <Text className="text-[14px] font-poppins text-black flex-1">
-          Remember Me
-        </Text>
-        <TouchableOpacity>
-          <Text className="text-[14px] font-poppins-semibold text-primary-900 underline">
-            Forgot password?
+        <Image source={icons.box} className="h-[18px] w-[18px] mr-4" />
+
+        <View className="flex-1">
+          <Text className="text-[14px] font-poppins text-black">
+            I agree to the{" "}
+            <Text className="text-primary-900 underline">
+              terms and conditions
+            </Text>{" "}
+            and
           </Text>
-        </TouchableOpacity>
+          <Text className="text-[14px] font-poppins text-primary-900 underline">
+            privacy policy
+          </Text>
+        </View>
       </View>
       {/* button */}
-      <FormButton title="Sign In" />
+      <FormButton title="Sign Up" />
       {/* or sign up with */}
       <View className="flex-row items-center justify-between mt-2 mb-5">
         <View className="w-[35%] h-[1px] bg-[#BDBDBD]" />
@@ -49,11 +55,6 @@ export default function Index() {
         <View className="h-[50px] w-[48%] bg-[#f5f5f5] justify-center items-center rounded-[12px]">
           <Image source={icons.facebook} className="h-[24px] w-[24px]" />
         </View>
-      </View>
-      <View>
-        <Text className="text-[14px] font-poppins text-center">
-          Don't have an account? Sign Up
-        </Text>
       </View>
     </SafeAreaView>
   );
--- components/formbutton.tsx
+++ components/formbutton.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
 
 interface FormInterface {
   title: string;
@@ -7,11 +7,14 @@ interface FormInterface {
 
 const FormButton = ({ title }: FormInterface) => {
   return (
-    <View className="h-[50px] bg-primary-900 rounded-[12px] items-center justify-center mb-3">
+    <TouchableOpacity
+      activeOpacity={0.6}
+      className="h-[50px] bg-primary-900 rounded-[12px] items-center justify-center mb-3"
+    >
       <Text className="text-[14px] text-white font-poppins-medium">
         {title}
       </Text>
-    </View>
+    </TouchableOpacity>
   );
 };
 
