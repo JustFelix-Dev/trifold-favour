f257c8ab4e8146c530bd4c07e524cb23f7e1bc52
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2025-12-01 16:09:52 +0100

    fixes the header

--- app/resources.tsx
+++ app/resources.tsx
@@ -1,15 +1,44 @@
 import icons from "@/constants/icons";
+import images from "@/constants/images";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Resources = () => {
+  const categoryData: string[] = [
+    "All",
+    "Articles",
+    "Books",
+    "Videos",
+    "Audios",
+  ];
+  const articlesData = [
+    {
+      id: 1,
+      title: "Strengthening Emotional Intimacy",
+      desc: 'Jesudamilare "JD" Adesegun-David',
+      image: images.image1,
+    },
+    {
+      id: 2,
+      title: "Strengthening Emotional Intimacy",
+      desc: 'Jesudamilare "JD" Adesegun-David',
+      image: images.image2,
+    },
+    {
+      id: 3,
+      title: "Strengthening Emotional Intimacy",
+      desc: 'Jesudamilare "JD" Adesegun-David',
+      image: images.image3,
+    },
+  ];
+
   return (
     <SafeAreaView className="flex-1 bg-[#fbf8fd]">
       <StatusBar style="dark" backgroundColor="#ffffff" />
       {/* header */}
-      <View className="bg-white flex-row items-center justify-between px-6 pt-3">
+      <View className="bg-white flex-row items-center justify-between px-6 pt-5 pb-2">
         <TouchableOpacity>
           <Image source={icons.arrowleft} className="h-[15px] w-[18px]" />
         </TouchableOpacity>
@@ -26,6 +55,35 @@ const Resources = () => {
             className="text-[14px] text-black ml-2"
             placeholder="Search"
           />
+        </View>
+        {/* categories */}
+        <View className="flex-row items-center justify-between mt-5">
+          {categoryData.map((item, index) => {
+            return (
+              <View
+                key={index}
+                className={`h-[25px] w-[60px] ${index === 0 ? "bg-primary-900" : "bg-white border-[1px] border-[#fbf8fd]"} rounded-[12px] items-center justify-center `}
+              >
+                <Text
+                  className={`text-[12px] ${index === 0 ? "text-white" : "text-primary-600"} font-poppins`}
+                >
+                  {item}
+                </Text>
+              </View>
+            );
+          })}
+        </View>
+        {/* articles */}
+        <View>
+          <View>
+            {articlesData.map((item, index) => {
+              return (
+                <View key={index}>
+                  <Image source={item.image} />
+                </View>
+              );
+            })}
+          </View>
         </View>
       </View>
     </SafeAreaView>
--- constants/images.ts
+++ constants/images.ts
@@ -1,5 +1,11 @@
 import assesment_detail from "../assets/images/assesment_detail.png";
+import image1 from "../assets/images/image1.png";
+import image2 from "../assets/images/image2.png";
+import image3 from "../assets/images/image3.png";
 
 export default {
   assesment_detail,
+  image1,
+  image2,
+  image3,
 };
